f848f89ae3ddc1822789f568100d7fa122d914d7
Arijus Šukys <arijuss@wix.com>
2021-08-17 10:03:10 +0300

    wix ui codemod/upgrade jscodeshift (#2192)
    * upgrade jscodeshift version
    
    it is required to make `wix-base-ui/destruct-imports` work
    
    * fix test fixture

--- packages/wix-ui-codemod/src/wix-style-react/__testfixtures__/migrate-wsr8/card-header.output.js
+++ packages/wix-ui-codemod/src/wix-style-react/__testfixtures__/migrate-wsr8/card-header.output.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { Card } from 'wix-style-react';
 
 const component = () => (
-  <Card>
+  (<Card>
     <Card.Header title="no divider" />
     <Card.Content>touch me</Card.Content>
     <Card.Header title="insert divider here" />
     <Card.Divider />
     <Card.Content>Dont touch me</Card.Content>
-  </Card>
+  </Card>)
 );
