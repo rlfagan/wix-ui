83ad7c9c94a69d5525e0f7cba4cfa6e31b2a58b7
Shachar Almagor <shachara@wix.com>
2021-10-24 13:37:44 +0300

    Signing commit

--- packages/wix-ui-codemod/src/wix-ui-tpa/__testfixtures__/replace-old-icon-imports.input.js
+++ packages/wix-ui-codemod/src/wix-ui-tpa/__testfixtures__/replace-old-icon-imports.input.js
@@ -3,7 +3,6 @@ import { ReactComponent as Calendar } from 'wix-ui-tpa/dist/src/assets/icons/Cal
 import { ReactComponent as bla } from 'wix-ui-tpa/dist/src/assets/icons/CaretDown.svg';
 import { ReactComponent as CaretUp } from 'wix-ui-tpa/dist/src/assets/icons/CaretUp.svg';
 import { ReactComponent as Check } from 'wix-ui-tpa/dist/src/assets/icons/Check.svg';
-import { ReactComponent as Youtube } from 'wix-ui-tpa/dist/src/assets/icons/Social/Youtube.svg';
 import thanos from 'the-dead';
 
 console.log('yo');
--- packages/wix-ui-codemod/src/wix-ui-tpa/__testfixtures__/replace-old-icon-imports.output.js
+++ packages/wix-ui-codemod/src/wix-ui-tpa/__testfixtures__/replace-old-icon-imports.output.js
@@ -3,7 +3,6 @@ import Calendar from 'wix-ui-icons-common/on-stage/Calendar';
 import bla from 'wix-ui-icons-common/on-stage/ArrowDown';
 import CaretUp from 'wix-ui-icons-common/on-stage/ArrowUp';
 import Check from 'wix-ui-icons-common/on-stage/Check';
-import Youtube from 'wix-ui-icons-common/on-stage/Youtube';
 import thanos from 'the-dead';
 
 console.log('yo');
