68ed918d36e78e4829d58bf3cd9ca0e30a5da872
TalZa1810 <24567980+TalZa1810@users.noreply.github.com>
2021-11-07 16:42:35 +0200

    adding visual tests

--- packages/wix-storybook-utils/src/IncludedComponents/test/IncludedComponents.visual.tsx
+++ packages/wix-storybook-utils/src/IncludedComponents/test/IncludedComponents.visual.tsx
@@ -1,17 +1,19 @@
-// import React from 'react';
-// import { storiesOf } from '@storybook/react';
-// import IncludedComponents from '..';
-//
-// const tests = [
-//   {
-//     it: `should render as=`,
-//     props: {
-//       as: 'value',
-//       children: `Heading`,
-//     },
-//   },
-// ] as const;
-//
-// tests.forEach(({ it, props }) => {
-//   storiesOf('Heading', module).add(it, () => <IncludedComponents {...props} />);
-// });
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import IncludedComponents from '..';
+
+const componentsList = [
+  { category: 'Category', title: 'FirstComponent', optional: true },
+  { category: 'Category', title: 'SecondComponent' },
+];
+
+const tests = [
+  {
+    it: `should render included components list`,
+    props: { componentsList },
+  },
+] as const;
+
+tests.forEach(({ it, props }) => {
+  storiesOf('IncludedComponents', module).add(it, () => <IncludedComponents {...props} />);
+});
