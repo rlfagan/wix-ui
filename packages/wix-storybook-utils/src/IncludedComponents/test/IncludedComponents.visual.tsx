80efeac8cb79ed8c8c936542c4c519e824d50c49
TalZa1810 <24567980+TalZa1810@users.noreply.github.com>
2021-11-09 13:56:13 +0200

    adding more visual test

--- packages/wix-storybook-utils/src/IncludedComponents/test/IncludedComponents.visual.tsx
+++ packages/wix-storybook-utils/src/IncludedComponents/test/IncludedComponents.visual.tsx
@@ -2,18 +2,29 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import IncludedComponents from '..';
 
-const includedComponents = [
-  { category: 'Category', title: 'FirstComponent', optional: true },
-  { category: 'Category', title: 'SecondComponent' },
-];
-
 const tests = [
   {
-    it: `should render included components list`,
-    props: { includedComponents },
+    it: `should render components list`,
+    props: {
+      includedComponents: [
+        { category: 'Category', title: 'FirstComponent' },
+        { category: 'Category', title: 'SecondComponent' },
+      ],
+    },
+  },
+  {
+    it: `should render optional components list`,
+    props: {
+      includedComponents: [
+        { category: 'Category', title: 'FirstComponent', optional: true },
+        { category: 'Category', title: 'SecondComponent', optional: true },
+      ],
+    },
   },
 ] as const;
 
 tests.forEach(({ it, props }) => {
-  storiesOf('IncludedComponents', module).add(it, () => <IncludedComponents {...props} />);
+  storiesOf('IncludedComponents', module).add(it, () => (
+    <IncludedComponents {...props} />
+  ));
 });
